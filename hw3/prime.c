1e02bdf5c635905b13c408bbc30585ce3485616c
Tan Chien <tachien@pp1.cs.nctu.edu.tw>
2017-11-28 09:01:07 +0800

    add long long

--- hw3/prime.c
+++ hw3/prime.c
@@ -3,10 +3,10 @@
 #include <math.h>
 #include <mpi.h>
 
-int isprime(long long n) {
-	int i, squareroot;
+int isprime(unsigned long long n) {
+	unsigned long long int i, squareroot;
 	if (n > 10) {
-		squareroot = (int)sqrt(n);
+		squareroot = (unsigned long long int)sqrt(n);
 		for (i = 3; i <= squareroot; i += 2)
 			if (n % i == 0)
 				return 0;
@@ -21,9 +21,9 @@ int main(int argc, char *argv[]) {
 	int rank, size;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	long long local_pc, pc;
-	long long local_foundone, foundone;
-	long long int n, limit;
+	unsigned long long int local_pc, pc;
+	unsigned long long int local_foundone, foundone;
+	unsigned long long int n, limit;
 	sscanf(argv[1],"%llu",&limit);
 	if (rank == 0)
 		printf("Starting. Numbers to be scanned= %lld\n", limit);
@@ -35,9 +35,9 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	MPI_Reduce(&local_pc, &pc, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-	MPI_Reduce(&local_foundone, &foundone, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&local_foundone, &foundone, 1, MPI_UNSIGNED_LONG_LONG, MPI_MAX, 0, MPI_COMM_WORLD);
 	if (rank == 0)
-		printf("Done. Largest prime is %lld Total primes %lld\n", foundone, pc + 4);
+		printf("Done. Largest prime is %llu Total primes %llu\n", foundone, pc + 4);
 	MPI_Finalize();
 	return 0;
 }
